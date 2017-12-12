81f5b3d22143100a0a7e88f3bb17080cf616955a
Korobov Yuriy <event3000@gmail.com>
2017-12-12 17:08:05 +0300

    ДЗ к 12 дек. сделано пока 1 задание

---
+++ homework/event/js/events.js
@@ -0,0 +1,41 @@
+(function(){
+'use strict';
+
+// ====================================================
+// 1. События клавиатуры keydown, keyup, keypress
+// Написать функцию обработчик события так, чтобы вводимая пользователем инфоромация сразу же отображалась в textarea или любом другом месте
+
+
+// захват эл-тов по id
+let fl1 = document.getElementById("field1");
+let fl2 = document.getElementById("field2");
+
+fl1.addEventListener("keyup", add); // на 1е поле вешаем обработчик
+
+// создаем функцию, которая к 2му эл-ту добавляет атрибут со значением  от 1го поля
+function add(event) {
+	fl2.setAttribute("value", fl1.value); 
+}	
+
+// ====================================================
+// 2. Написать по одной любой функции-обработчику на следующие события:
+// DOMContentLoaded
+// beforeunload
+// onload на отдельный элемент
+
+document.addEventListener("DOMContentLoaded", domCont);
+
+function domCont(event) {
+    console.log("DOMContentLoaded:", "браузер полностью загрузил HTML и построил DOM-дерево");
+};
+
+// ОСТАЛЬНЫЕ в процессе
+
+
+
+
+
+
+
+}() );
+
